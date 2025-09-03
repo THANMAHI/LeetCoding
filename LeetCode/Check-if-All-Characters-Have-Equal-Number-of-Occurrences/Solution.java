6401eb762e939b7d0643af9efc3c668a31d5b807
THANMAHI <147476157+THANMAHI@users.noreply.github.com>
2025-09-03 15:36:42 +0530

    Add solution for Check if All Characters Have Equal Number of Occurrences

---
+++ LeetCode/Check-if-All-Characters-Have-Equal-Number-of-Occurrences/Solution.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public boolean areOccurrencesEqual(String s) {
+        int[] freq = new int[26]; 
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++; 
+        }
+        int count = 0;
+        for (int f : freq) {
+            if (f > 0) {
+                if (count == 0) {
+                    count = f; 
+                } else if (f != count) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
